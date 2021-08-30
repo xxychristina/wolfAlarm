34e41a8e54c0bde2e06de2ebd4dfb39f23afd712
table1065 <41575967+table1065@users.noreply.github.com>
2021-08-30 15:48:10 +1000

    Added header and map to home page

--- App.js
+++ App.js
@@ -1,22 +1,72 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { Header } from "react-native-elements/dist/header/Header";
+import { SafeAreaView } from "react-native";
+import MapView from "react-native-maps";
+import { useFonts, Sofia_400Regular } from "@expo-google-fonts/sofia";
+import { TouchableNativeFeedback } from "react-native";
+
+// Component
+// import WolfAlarmHeader from "./components/Header/WolfAlarmHeader";
 
 // View -> UIView
 export default function App() {
+  let [fontLoaded] = useFonts({
+    Sofia_400Regular,
+  });
   return (
-    <View style={styles.container}>
-      <Text>Hello World</Text>
+    <SafeAreaView style={styles.container}>
       <StatusBar style="auto" />
-    </View>
+      {fontLoaded && (
+        <Header
+          centerComponent={{
+            text: "Wolf Alarm",
+            style: styles.heading,
+          }}
+          containerStyle={styles.headerContainer}
+        />
+      )}
+      <MapView style={styles.map} />
+      <TouchableNativeFeedback onPress={() => console.warn("Hello")}>
+        <View style={styles.sosButton}>
+          <Text>SOS</Text>
+        </View>
+      </TouchableNativeFeedback>
+    </SafeAreaView>
   );
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#f8f8f8",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+  map: {
+    flexDirection: "row",
+    width: "100%",
+    height: "50%",
+    position: "absolute",
+    top: 60,
+  },
+
+  heading: {
+    color: "#fff",
+    fontFamily: "Sofia_400Regular",
+    fontSize: 20,
+  },
+
+  headerContainer: {
+    flexDirection: "row",
+    backgroundColor: "#4A5C72",
+    position: "absolute",
+    top: 20,
+  },
+
+  sosButton: {
+    position: "absolute",
+    top: 600,
   },
 });
