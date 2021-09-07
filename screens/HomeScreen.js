0a62bf35599387b39d5433560654d7892216ca46
table1065 <41575967+table1065@users.noreply.github.com>
2021-09-07 17:28:09 +1000

    Added alarm

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
 import { SafeAreaView } from "react-native";
 import MapView from "react-native-maps";
 import { TouchableOpacity } from "react-native";
+import { Audio } from "expo-av";
 // import Torch from "react-native-torch";
 // import RNImmediatePhoneCall from "react-native-immediate-phone-call";
 // import AudioRecord from "react-native-audio-record";
@@ -15,12 +16,26 @@ const options = {
   wavFile: "test.wav", // default 'audio.wav'
 };
 
+const sound = new Audio.Sound();
+sound.loadAsync(require("../sound/alarm.mp3"));
 export default function HomeScreen({ navigation }) {
+  const [alarmPlaying, setAlarmPlaying] = useState(false);
   const SOSPressHandler = () => {
     // TODO: Complete the SOS function
   };
-  const AFPressHandler = () => {
+  const AFPressHandler = async () => {
     // TODO: Complete the alarm and flash function
+    if (alarmPlaying) {
+      console.log("Stop");
+      await sound.pauseAsync();
+      setAlarmPlaying(false);
+    } else {
+      setAlarmPlaying(true);
+      console.log("Playing sound");
+      sound.setIsLoopingAsync(true);
+      sound.setVolumeAsync(1);
+      await sound.playAsync();
+    }
   };
   const VoicePressHandler = () => {
     navigation.navigate("Voice");
@@ -36,7 +51,7 @@ export default function HomeScreen({ navigation }) {
         <TouchableOpacity style={styles.button} onPress={SOSPressHandler}>
           <Text style={styles.buttonText}>SOS</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={styles.button}>
+        <TouchableOpacity style={styles.button} onPress={AFPressHandler}>
           <Text style={styles.buttonText}>Alarm/Flash</Text>
         </TouchableOpacity>
       </View>
