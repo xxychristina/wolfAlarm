e1820c46bcbd89fc60d205763bdf2cd9e5735682
Hhhhoooope <2230172939@qq.com>
2021-09-11 23:54:47 +1000

    map tracking basically working

--- components/BushMap.js
+++ components/BushMap.js
@@ -1,6 +1,7 @@
-import React, { Component } from 'react';
+import React, { Component, useEffect, useState } from 'react';
 import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
 import  MapView, { PROVIDER_GOOGLE, Marker } from "react-native-maps";
+import * as Location from 'expo-location'
 //import { MapView } from 'expo';
 //react-native-maps
 
@@ -8,51 +9,78 @@ import  MapView, { PROVIDER_GOOGLE, Marker } from "react-native-maps";
 
 export default class BushMap extends Component {
     constructor(props) {
-        super();
-        this.mapRef = null;
+        super(props);
+        this.state= {
+            latitude: 0,
+            longitude: 0,
+            error: null,
+            mapRegion: null,
+            lastLat: null,
+            lastLong: null,
+        }
+    };
+
+    componentDidMount() {
+        const perssion = Location.requestForegroundPermissionsAsync();
+        const getper = Location.getForegroundPermissionsAsync();
+        Location.installWebGeolocationPolyfill()
+        this.watchId = navigator.geolocation.watchPosition(
+          (position) => {
+            let region = {
+                latitude:       position.coords.latitude,
+                longitude:      position.coords.longitude,
+                latitudeDelta:  0.00922*1.5,
+                longitudeDelta: 0.00421*1.5
+            };
+            this.setState({
+              latitude: position.coords.latitude,
+              longitude: position.coords.longitude,
+              mapRegion: region,
+              error: null,
+            });
+          },
+          (error) => this.setState({ error: error.message }),
+          { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000, distanceFilter: 10 },
+        );
+        
+      }
+
+    componentWillUnmount() {
+        navigator.geolocation.clearWatch(this.watchId);
     }
 
     render() {
         return (
-            <MapView
-                initialRegion={{
-                    latitude: 37.78825,
-                    longitude: -122.4323,
-                    latitudeDelta: 0.015,
-                    longitudeDelta: 0.0121,
-                }}
-                name="homePageMap" 
-                style={styles.map}
-                // provider={PROVIDER_GOOGLE}
-                ref={(ref) => { this.mapRef = ref }}
-                onLayout = {() => this.mapRef.fitToCoordinates(this.props.myLatLongs, { edgePadding: { top: 30, right: 30, bottom: 30, left: 30 }, animated: true })}
-                showsUserLocation={true}
-            >
-            </MapView>
+            <React.Fragment>
+                <MapView
+                    name="homePageMap" 
+                    style={styles.map}
+                    initialRegion={this.state.mapRegion}
+                    showsUserLocation={true}
+                    followUserLocation={true}
+                >
+                    {/* <Marker
+                        coordinate={ this.state }
+                        title="test Title"
+                        description="test describtion"
+
+                    >
+                    </Marker> */}
+                </MapView>
+                <View style={{ flexGrow: 1, alignItems: 'center', justifyContent: 'center' }}>
+                    <Text>Latitude: {this.state.latitude}</Text>
+                    <Text>Longitude: {this.state.longitude}</Text>
+                    {this.state.error ? <Text>Error: {this.state.error}</Text> : null}
+                </View>
+                
+            </React.Fragment>
         )
     }
 }
-// 
-//                 <Marker
-//                     coordinate={{
-//                         latitude: 37.78825,
-//                         longitude: -122.4323,
-                        
-//                     }}
-//                     image={require('../assets/maps-and-flags.png')}
-//                     title="test Title"
-//                     description="test describtion"
-//                 >
-//                 </Marker>
+
+
 
 const styles = StyleSheet.create({
-    container: {
-        flexDirection: "column",
-        backgroundColor: "#f8f8f8",
-        alignItems: "center",
-        justifyContent: "flex-start",
-    },
-
     map: {
         width: "100%",
         height: "75%",
