d8b4bdd2be85f6e9aa7b3fa5f05553ab38f383be
Christina <xxychristina@gmail.com>
2021-09-12 21:25:41 +0800

    comming soon

--- screens/SOSScreen.js
+++ screens/SOSScreen.js
@@ -4,7 +4,7 @@ import { SafeAreaView, StyleSheet, Text } from "react-native";
 export default function SOSScreen() {
   return (
     <SafeAreaView style={styles.container}>
-      <Text style={styles.text}>SOS</Text>
+      <Text style={styles.text}>up comming</Text>
     </SafeAreaView>
   );
 }
--- screens/VirtualCallScreen.js
+++ screens/VirtualCallScreen.js
@@ -4,7 +4,7 @@ import { SafeAreaView, StyleSheet, Text } from "react-native";
 export default function VirtualCallScreen() {
   return (
     <SafeAreaView style={styles.container}>
-      <Text style={styles.text}>VirtualCall</Text>
+      <Text style={styles.text}>Up comming</Text>
     </SafeAreaView>
   );
 }
