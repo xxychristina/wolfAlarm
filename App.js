f5f0562d224e5ad09f4d8b2804a095020fc3482f
housegoing <wxu551873@gmail.com>
2022-06-27 22:55:06 +1000

    initial commit

--- App.js
+++ App.js
@@ -1,90 +1,20 @@
-import React from "react";
-import { TouchableOpacity } from "react-native";
-import { NavigationContainer, useRoute } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
-import { useFonts, Sofia_400Regular } from "@expo-google-fonts/sofia";
-import { getFocusedRouteNameFromRoute } from "@react-navigation/native";
-import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
-// Screens
-import VoiceScreen from "./screens/VoiceScreen";
-import VirtualCallScreen from "./screens/VirtualCallScreen";
-import EContact from "./screens/EContact";
-import Help from "./screens/Help";
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
 
-// Navigators
-import HomeTabNavigator from "./navigators/HomeTabNavigator";
-
-// const Tab = createBottomTabNavigator();
-const Stack = createStackNavigator();
-const header = {
-  headerStyle: { backgroundColor: "#4A5C72" },
-  headerTitleStyle: {
-    color: "white",
-    fontFamily: "Sofia_400Regular",
-  },
-  headerTitleAlign: "center",
-};
-
-// View -> UIView
 export default function App() {
-  let [fontLoaded] = useFonts({
-    Sofia_400Regular,
-  });
-
-  const inviteButton = {
-    headerStyle: { backgroundColor: "#4A5C72" },
-    headerTitleStyle: {
-      color: "white",
-      fontFamily: "Sofia_400Regular",
-    },
-    headerTitleAlign: "center",
-    headerRight: () => (
-      <TouchableOpacity style={{ alignSelf: "flex-end", paddingRight: 20 }}>
-        <MaterialCommunityIcons
-          size={26}
-          name="account-multiple-plus-outline"
-        ></MaterialCommunityIcons>
-      </TouchableOpacity>
-    ),
-  };
-
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
-          name="Wolf Alarm"
-          component={HomeTabNavigator}
-          options={({ route }) => {
-            const routeName =
-              getFocusedRouteNameFromRoute(route) ?? "Wolf Alarm";
-            if (routeName === "HomeTab") {
-              return header;
-            } else {
-              return { headerShown: false };
-            }
-          }}
-        />
-        <Stack.Screen
-          name="Voice"
-          component={VoiceScreen}
-          options={fontLoaded && header}
-        />
-        <Stack.Screen
-          name="VirtualCall"
-          component={VirtualCallScreen}
-          options={fontLoaded && header}
-        />
-        <Stack.Screen
-          name="Emergency Contact"
-          component={EContact}
-          options={fontLoaded && header}
-        />
-        <Stack.Screen
-          name="Help"
-          component={Help}
-          options={fontLoaded && header}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
